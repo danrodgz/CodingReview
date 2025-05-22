9b904e40cd33aca80f33716812220dc30c4754a1
Daniel Sebastian Álvarez Rodríguez <daniel.alvarezro96@gmail.com>
2025-05-22 14:40:00 -0600

    Se corrige código y se añade README

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-* Coding Review 
+# Módulo "Revisión de código de otros 1"
+
+## Ejercicio
+
+**Leer con atención el código y analizar para poder entender su funcionamiento. Contiene errores.**
+
+* Obtener información API
+* Mostrar en DOM
+* Mejorar legibilidad
+* Funcionamiento de código
+* Documentación de código con comentarios
+
+---
+
+*Revisión de código de otros - Generation CH53*
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.querySelector('.name'); // Se le añade '.' para referirse a la clase
+const $b = document.querySelector('.blog'); // Se le añade '.' para referirse a la clase
+const $l = document.querySelector('.location'); // Se le añade '.' para referirse a la clase
 
-function displayUser(username) {
+async function displayUser(username) { // Se añade 'async' para poder usar la expresión 'await'
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  try { // Se añade la declaración 'try / catch' para señalar el bloque de instrucciones a ejecutar
+      const response = await fetch(`${usersEndpoint}/${username}`);
+      const data = await response.json(); // Se obtiene la respuesta en formato JSON
+      $n.textContent = `${data.name}`; // Cambio de comillas simples a backticks
+      $b.textContent = `${data.blog}`; // Cambio de comillas simples a backticks
+      $l.textContent = `${data.location}` // Cambio de comillas simples a backticks
+  } catch (err) {
+    handleError (err);
+  } //catch
+} //function displayUser
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}` // Se le añade el '$' al inicio para cumplir la sintáxis de la constante a llamar
 }
 
-displayUser('stolinski').catch(handleError);
+ displayUser('stolinski').catch(handleError);
